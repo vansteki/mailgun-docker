d59b7407546fc03e541a10c76102bb7a1805f4d8
vansteki <vansteki.tw@gmail.com>
2014-10-28 13:33:22 +0800

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@
 	allowCrossDomain = function(req, res, next) {
 		res.header("Access-Control-Allow-Origin", "*")
 		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-    	res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE')
+		res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE')
     	res.header("Content-Type", "application/json; charset=utf-8")
     	next();
 	}
