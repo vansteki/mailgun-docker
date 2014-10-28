27454e8b03ef6ca555b0610fa922d2a8f1ebfbfe
vansteki <vansteki.tw@gmail.com>
2014-10-28 13:32:47 +0800

    Update app.js

--- app.js
+++ app.js
@@ -10,8 +10,8 @@
 	attachFileName = undefined
 	attachFilePath = '/data/'
 	allowCrossDomain = function(req, res, next) {
-    	res.header("Access-Control-Allow-Origin", "*")
-    	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+		res.header("Access-Control-Allow-Origin", "*")
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     	res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE')
     	res.header("Content-Type", "application/json; charset=utf-8")
     	next();
